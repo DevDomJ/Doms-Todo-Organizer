cd2f38b0ed233f7928a9e47d50c11d9b47d5179f
Dominik Jung <jung.dominik.dj@gmail.com>
2023-02-24 22:48:56 +0100

    Remove example code

--- src/views/TodoOrganizerView.vue
+++ src/views/TodoOrganizerView.vue
@@ -3,18 +3,12 @@ import TodoList from '../components/TodoList.vue'
 </script>
 
 <template>
-  <div class="about">
+  <div>
     <h1>This is where it begins</h1>
     <TodoList />
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+@media (min-width: 1024px) {}
 </style>
